16806ffccd95f509dcb77c8329f755921ae426e5
dhcdht <dhcdht@qq.com>
2016-12-31 11:11:18 +0800

    连续生成障碍物

--- flappybird/assets/Script/PillarGenerator.js
+++ flappybird/assets/Script/PillarGenerator.js
@@ -33,31 +33,60 @@ cc.Class({
             tooltip: "障碍物的移动速度",
         },
 
+        pillarGenerateMargin: {
+            default: 0,
+            tooltip: "间隔多远生成一次障碍物",
+        },
+
         pillars: {
             default: [],
+            visible: false,
             tooltip: "记录所有障碍物的数组",
+        },
+
+        lastPillarGenerateMargin: {
+            default: 0,
+            visible: false,
+            tooltip: "距离上一次生成障碍物间隔了多远了",
         }
     },
 
     // use this for initialization
     onLoad: function () {
-        let pillarArr = this.generateTwoPillar();
-
-        this.pillars.push(pillarArr);
+        this.generateTwoPillar();
     },
 
     // called every frame, uncomment this function to activate update callback
     update: function (dt) {
+        // 本次更新移动的距离
+        let dtSpace = dt * this.pillarMoveSpeed;
+
+        // 移动各个障碍物
         for (let i = 0; i < this.pillars.length; i++) {
             let pillarArr = this.pillars[i];
             let upPillar = pillarArr[0];
             let downPillar = pillarArr[1];
 
-            upPillar.x -= dt * this.pillarMoveSpeed;
-            downPillar.x -= dt * this.pillarMoveSpeed;
+            upPillar.x -= dtSpace;
+            downPillar.x -= dtSpace;
         }
+
+        // 记录距离上一次生成障碍物间隔了多远了
+        this.lastPillarGenerateMargin += dtSpace;
+
+        // 检查是否需要生成新的障碍物
+        if (this.lastPillarGenerateMargin >= this.pillarGenerateMargin) {
+            // TODO：这里会有误差的应该
+            this.generateTwoPillar();
+            this.lastPillarGenerateMargin = 0;
+        }
+
+        // TODO: 下边要有离开屏幕的障碍物的清理
     },
 
+    /**
+     * 生成一对新的障碍物
+     */
     generateTwoPillar: function () {
         // 障碍物间隙中心位置上下
         // 这里已经照顾到了上下障碍物都有个最小高度
@@ -75,6 +104,7 @@ cc.Class({
         // 设置上边障碍物的位置大小
         upPillar.height = upPillarHeight;
         upPillar.setPositionY(upPillarDownY + upPillarHeight / 2);
+        upPillar.setPositionX(this.node.width / 2 + upPillar.width / 2);
 
         // 下边的柱子障碍物
         let downPillar = cc.instantiate(this.pillarPrefab);
@@ -88,7 +118,9 @@ cc.Class({
         // 设置下边障碍物的位置大小
         downPillar.height = downPillarHeight;
         downPillar.setPositionY(downPillarUpY - downPillarHeight / 2);
+        downPillar.setPositionX(this.node.width / 2 + downPillar.width / 2);
 
-        return [upPillar, downPillar];
+        // 放到障碍物数组中
+        this.pillars.push([upPillar, downPillar]);
     },
 });
